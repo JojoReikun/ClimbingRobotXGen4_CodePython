5ebab52f48d40123e05febbd5e412c82302e6125
JojoReikun <jojo.schultz@outlook.de>
2019-08-23 13:02:15 +1000

    first commit, project scaffold

---
+++ climbingRobot_communication.py
@@ -0,0 +1,1 @@
+import climbingRobot_html
---
+++ climbingRobot_main.py
@@ -0,0 +1,6 @@
+import climbingRobot_Locomotion
+import climbingRobot_communication
+import climbingRobot_html
+
+# remember to activate MicroPython and ESP support (Settings -> Languages & Frameworks...)
+
